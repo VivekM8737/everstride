60f0f93269f37b547ce19278c7ef4bce42b86d8c
Vivek <avmishra1438@gmail.com>
2025-09-07 23:45:47 +0530

    fix some design

--- src/Components/Services.tsx
+++ src/Components/Services.tsx
@@ -10,27 +10,25 @@ export default function Services() {
             </section>
             <div>
                 {/* Core Areas */}
-                <div className="py-16 px-6 md:px-20">
-                    <h2 className="text-3xl bg-blue-100 font-semibold text-center text-blue-900 mb-12">
+                    <h2 className="text-3xl my-4 bg-blue-100 font-semibold text-center text-blue-900 mb-12">
                         What We Do
                     </h2>
+                <div className="px-6 md:px-20">
 
-                    <div className="bg-white shadow-lg text-center rounded-xl p-6 border-t-4 border-blue-900 mb-6 ">
+                    <div className="bg-white shadow-lg rounded-xl p-6 border-t-4 border-blue-900 mb-6  text-center">
                         <h3 className="text-2xl text-center font-bold mb-4 text-blue-800">Advisory</h3>
-                        <div>
+                        
+                            <p className="text-gray-700   text-m mb-1">
+                                •	Partner with companies to design sports-driven growth strategies
+                            </p>
+                            <p className="text-gray-700 text-m mb-1">
+                                •	Focus on creating value and driving innovation through sports
+                            </p>
+                            <p className="text-gray-700 text-m mb-1">
+                                •	Serve as a long-term growth partner, not just a short-term consultant
 
-                       
-                        <p className="text-gray-700   text-m mb-1">
-                            •	Partner with companies to design sports-driven growth strategies
-                        </p>
-                        <p className="text-gray-700 text-m mb-1">
-                            •	Focus on creating value and driving innovation through sports
-                        </p>
-                        <p className="text-gray-700 text-m mb-1">
-                            •	Serve as a long-term growth partner, not just a short-term consultant
-
-                        </p>
-                         </div>
+                            </p>
+                        
 
                     </div>
 
